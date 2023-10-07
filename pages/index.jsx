b0ec61088bec2e50298300f1be61bfbccb517a8c
Tony <extracat@users.noreply.github.com>
2023-10-07 04:37:49 +0200

    save

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,26 @@
-export default function About() {
-  return <div>About</div>
+// TODO: Need to fetch `posts` (by calling some API endpoint)
+//       before this page can be pre-rendered.
+export default function Blog({ posts }) {
+  return (
+    <ul>
+      {posts.map((post) => (
+        <li>{post.title}</li>
+      ))}
+    </ul>
+  )
+}
+ 
+// This function gets called at build time
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts
+  const res = await fetch('https://local')
+  const posts = await res.json()
+ 
+  // By returning { props: { posts } }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      posts,
+    },
+  }
 }
