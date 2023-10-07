040f5b5793db4f83cde6aa936c8ec2569a1d23bb
Tony <extracat@users.noreply.github.com>
2023-10-07 04:52:47 +0200

    static props

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,10 @@
 // TODO: Need to fetch `posts` (by calling some API endpoint)
 //       before this page can be pre-rendered.
-export default function Blog({ posts }) {
+export default function Index({ telegrams }) {
   return (
     <ul>
-      {posts.map((post) => (
-        <li>{post.title}</li>
+      {telegrams.map((telegram) => (
+        <li key={telegram._id}>{telegram.title}</li>
       ))}
     </ul>
   )
@@ -13,14 +13,14 @@ export default function Blog({ posts }) {
 // This function gets called at build time
 export async function getStaticProps() {
   // Call an external API endpoint to get posts
-  const res = await fetch('https://local')
-  const posts = await res.json()
+  const res = await fetch('https://astro-data-api.vercel.app/api/v1/telegrams')
+  const telegrams = await res.json()
  
   // By returning { props: { posts } }, the Blog component
   // will receive `posts` as a prop at build time
   return {
     props: {
-      posts,
+      telegrams,
     },
   }
 }
