7a06011151d63985d77d00164004dbc312a10f18
Tony@WorkBook <extracat@users.noreply.github.com>
2023-10-23 18:03:16 +0200

    save

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -4,9 +4,8 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        
+
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.css" integrity="sha384-n8MVd4RsNIU0tAv4ct0nTaAbDJwPJzDEaqSD1odI+WdtXRGWt2kTvGFasHpSy3SV" crossorigin="anonymous" />
- 
 
       </Head>
       <body className="px-8 py-8 antialiased bg-white dark:bg-black">
