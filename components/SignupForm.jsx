603690be15aa08e451d623a12cce8e38509e87ee
Tony@WorkBook <extracat@users.noreply.github.com>
2023-11-21 15:41:17 +0100

    signup done

--- components/SignupForm.jsx
+++ components/SignupForm.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-export function SigninForm ({ formData, handleChange, handleSubmit, isLoading }) {
-
+export function SignupForm({ formData, handleChange, handleSubmit, isLoading }) {
 
   return (
     <form onSubmit={handleSubmit}>
-
 
       <div className="form-control my-2">
         <label className="label">
@@ -20,7 +18,7 @@ export function SigninForm ({ formData, handleChange, handleSubmit, isLoading })
           onChange={handleChange}
           placeholder="Email"
         />
-      </div> 
+      </div>
 
       <div className="form-control my-2">
         <label className="label">
@@ -35,19 +33,33 @@ export function SigninForm ({ formData, handleChange, handleSubmit, isLoading })
           onChange={handleChange}
           placeholder="Password"
         />
-      </div> 
-      
-      <button className="btn-primary"
-        type="submit" disabled={isLoading}>
-        {isLoading ? 'Loading...' : 'Signin'}
+      </div>
+
+      <div className="form-control my-2">
+        <label className="label">
+          <span className="label-text">
+            Confirm Password:
+          </span>
+        </label>
+        <input
+          type="password"
+          name="confirmPassword"
+          value={formData.confirmPassword}
+          onChange={handleChange}
+          placeholder="Confirm Password"
+        />
+      </div>
+
+      <button className="btn-primary" type="submit" disabled={isLoading}>
+        {isLoading ? 'Loading...' : 'Signup'}
       </button>
 
       {/* display error */}
       {formData.error && <div className="alert alert-error my-4">
         <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
         <span>{formData.error}</span>
-      </div>} 
-      
+      </div>}
+
     </form>
   );
 }
---
+++ pages/signup.jsx
@@ -0,0 +1,138 @@
+const api = new (require('../controllers/api'))();
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
+import { SignupForm } from '../components/SignupForm';
+import { Header } from '../components/Header';
+import { Footer } from '../components/Footer';
+
+export default function SigninFormPage() {
+
+  const [isLoading, setIsLoading] = useState(false); // state to handle loading indicator
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+    error: ""
+  });
+
+
+  function getPostData(data) {
+    const { error, confirmPassword, ...postData } = data;
+    return postData;
+  }
+
+  const router = useRouter(); // initialize router
+
+  const validateFormData = () => {
+    if (formData.password !== formData.confirmPassword) {
+      return "Passwords do not match";
+    }
+
+    return null;
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true); // activate loading indicator
+    setFormData(prev => ({ ...prev, error: '' })); // clear error state
+
+    const validationError = validateFormData();
+    if (validationError) {
+      setFormData(prev => ({ ...prev, error: validationError }));
+      setIsLoading(false);
+      return;
+    }
+
+    const dataToSend = getPostData(formData);
+
+    console.log(dataToSend);
+
+    try {
+      const res = await api.post('/v1/signup', dataToSend)
+
+      if (!res.ok) {
+        let errorData;
+        const contentType = res.headers.get('content-type');
+
+        // Check if the response content type is JSON
+        if (contentType && contentType.indexOf('application/json') !== -1) {
+          errorData = await res.json(); // if it is JSON, then parse it
+        } else {
+          errorData = { error: await res.text() }; // if not, just get it as text
+        }
+        throw JSON.stringify(errorData) || 'Failed to signup'; // throw an error to catch it later
+      }
+
+      // Assuming here successful response will be JSON
+      //const data = await res.json(); // consider adding checking here as well
+      // Handle successful response if needed
+
+      const data = await res.json();
+
+
+      document.cookie = `token=${data.token}; path=/; max-age=86400;`;
+      //document.cookie = `token=${data.token}; path=/;`;
+
+      console.log('User logged in and token saved in cookies');
+      router.push('/'); // redirect the user to the homepage after successful submission
+
+    } catch (error) {
+      setFormData(prev => ({ ...prev, error: error.toString() })); // set the error to show it to the user
+    } finally {
+      setIsLoading(false); // deactivate loading indicator regardless of the outcome
+    }
+  };
+
+
+  const handleChange = (e, property = null, index = null, nestedProperty = null) => {
+    const { name, value } = e.target;
+
+    if (property) {
+      let updatedProperty = Array.isArray(formData[property]) ? [...formData[property]] : { ...formData[property] };
+      
+      if (Array.isArray(formData[property])) {
+        if (typeof updatedProperty[index] === 'object' && updatedProperty[index] !== null) {
+            // if it's an object
+            updatedProperty[index][name] = value;
+        } else {
+            // if it's a string
+            updatedProperty[index] = value;
+        }
+      } else if (nestedProperty) {
+        updatedProperty[nestedProperty][name] = value;
+      } else {
+        updatedProperty[name] = value;
+      }
+      setFormData(prev => ({ ...prev, [property]: updatedProperty })); 
+    } else {
+      setFormData(prev => ({ ...prev, [name]: value })); 
+    }
+  };
+
+  return (
+    <div>
+      <Header />
+      <h1>Sign Up</h1>
+      <SignupForm formData={formData} handleChange={handleChange} handleSubmit={handleSubmit} isLoading={isLoading}/>
+      <Footer />
+    </div>
+  );
+}
+
+// This function gets called at build time on server-side.
+// It may be called again, on a serverless function, if
+// revalidation is enabled and a new request comes in
+export async function getStaticProps() {
+ 
+
+  return {
+    props: {
+      initialData: {},
+    },
+    
+    // Next.js will attempt to re-generate the page:
+    // - When a request comes in
+    // - At most once every 10 seconds
+    revalidate: 10, // In seconds
+  }
+}
