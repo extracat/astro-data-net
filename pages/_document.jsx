60d59e0ece5381cf7bb9cd186cd9790290553835
Tony <extracat@users.noreply.github.com>
2024-04-07 17:25:44 +0200

    switched to google fonts

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -5,13 +5,17 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.css" integrity="sha384-n8MVd4RsNIU0tAv4ct0nTaAbDJwPJzDEaqSD1odI+WdtXRGWt2kTvGFasHpSy3SV" crossOrigin="anonymous" />
-        <link rel="preconnect" href="https://rsms.me/" />
-        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet"/>
+
       </Head>
-      <body className="px-8 py-8 adn-color-fill-bg">
+      <body className="px-8 py-8 antialiased adn-color-fill-bg">
         <article className="
                     container 
-                    prose sm:prose-lg dark:prose-invert prose-adn-color 
+                    prose dark:prose-invert prose-adn-color
+                    text-sm sm:text-base
                     prose-h1:text-center
                     prose-h1:text-2xl sm:prose-h1:text-3xl
                     prose-h1:leading-snug 
