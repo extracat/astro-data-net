e8a8215b25d80ca58e4a504c564497208d3af50b
Tony <extracat@users.noreply.github.com>
2024-04-17 21:05:38 +0200

    Analytics SpeedInsights modules added

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,15 @@
 import '../styles/global.css';
-
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+ 
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+      <SpeedInsights />
+    </>
+  );
 }
+ 
+export default MyApp;
