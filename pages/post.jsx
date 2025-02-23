d2f8842534a3f056b6d333f946a9a5fbd994124b
Tony at Home <extracat@users.noreply.github.com>
2025-02-23 08:30:21 +0100

    save

--- pages/post.jsx
+++ pages/post.jsx
@@ -7,88 +7,16 @@ import Footer from '../components/Footer';
 const api = new (require('../controllers/api'))();
 
 const initialData = {
-  _id: "6317b4f63f8aba7474aab97c",
-  adn_id: "ADN220903A",
-  user_id: "6317b4f63f8aba7474aab97c",
-  timestamp: "2025-02-22T18:58:47.067Z",
-  event_datetime: "2025-02-22T18:58:47.067Z",
-  title: "string",
-  authors: "string",
   authors_list: [
-    {
-      _id: "6317b4f63f8aba7474aab97c",
-      name: "string",
-      email: "user@example.com",
-      org: "string"
-    }
+    {}
   ],
-  body: "string",
   light_curve: [
-    {
-      coordinates: {
-        right_ascension: 359,
-        declination: 90,
-        error: 180
-      },
-      datetime: "2025-02-22T18:58:47.067Z",
-      magnitude: 0,
-      upper_limit: 0,
-      exptime: 0,
-      instrument: {
-        _id: "6317b4f63f8aba7474aab97c",
-        name: "string",
-        observation_mode: "string",
-        observatory: {
-          _id: "6317b4f63f8aba7474aab97c",
-          name: "string",
-          org: "string",
-          country: "string"
-        }
-      },
-      filter: "string"
-    }
+    {}
   ],
   upper_limits: [
-    {
-      framae: [
-        {
-          right_ascension: 359,
-          declination: 90
-        },
-        {
-          right_ascension: 359,
-          declination: 90
-        },
-        {
-          right_ascension: 359,
-          declination: 90
-        }
-      ],
-      datetime: "2025-02-22T18:58:47.067Z",
-      upper_limit: 0,
-      exptime: 0,
-      instrument: {
-        _id: "6317b4f63f8aba7474aab97c",
-        name: "string",
-        observation_mode: "string",
-        observatory: {
-          _id: "6317b4f63f8aba7474aab97c",
-          name: "string",
-          org: "string",
-          country: "string"
-        }
-      },
-      filter: "string"
-    }
+    {}
   ],
-  band: "string",
-  categories: [
-    {
-      "_id": "6317b4f63f8aba7474aab97c",
-      "text": "string",
-      "color": "string"
-    }
-  ],
+  band: "optical",
   references: [
     ""
   ]
